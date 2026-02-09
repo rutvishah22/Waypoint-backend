c595d886201333f2cfcbb740f0e68224d4750829
rutvi shah <rutvi0422@gmail.com>
2026-02-09 11:23:54 +0530

    made changes in main file

--- app/main.py
+++ app/main.py
@@ -54,11 +54,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
+        # "http://localhost:5173",
+        # "http://localhost:3000",
         "https://waypoint-jy2mvlsxi-rshahs-projects.vercel.app"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
